ee36bace33e2aabd336d316e07dddab8f8279d81
Mazzeo Alejandro <ale.exactas@gmail.com>
2017-06-15 01:37:23 -0300

    Analisis de resultados con distribucion bimodal funcionando e integrado a la clase Results

--- src/analysis.py
+++ src/analysis.py
@@ -91,20 +91,6 @@ class Analysis():
         if self._current - n > 0:
             self.current -= n
 
-class Result():
-    def __init__(self, sample_name):
-        self._sample_name = sample_name
-
-    @property
-    def magnet_array(self):
-        sample = Sample.load(self._sample_name)
-        return sample.magnet
-
-    @property
-    def energy_array(self):
-        sample = Sample.load(self._sample_name)
-        return sample.energy
-
 class Tools():
 
     @classmethod
@@ -154,7 +140,7 @@ class Tools():
 
     @classmethod
     def estimate_pdf(cls, data, plot=False, ax=None):
-        y, x = np.histogram(data, bins='sqrt', density=True)
+        y, x = np.histogram(data, bins='auto', density=True)
         x = (x[1:] + x[:-1]) / 2
         f = gaussian_kde(data)
         if plot:
@@ -185,10 +171,6 @@ class Tools():
 
         return peakind, x[peakind], y_pdf[peakind]
 
-#    @classmethod
-#    def gauss(cls, x, mu, sigma, A):
-#        return A*np.exp(-(x-mu)**2/2/sigma**2)
-
     @classmethod
     def gauss(cls, x, mu, sigma, A):
         return A * norm.pdf(x, loc=mu, scale=sigma)
@@ -204,8 +186,8 @@ class Tools():
 
     @classmethod
     def bimodal_expnorm(cls, x, mu, sigma1, sigma2, lamb1, lamb2, A, dA):
-        return (cls.expnorm(x, mu, sigma1, lamb1, A*dA) +
-                cls.expnorm(1-x, 1+mu, sigma2, lamb2, A*(1-dA)))
+        return (cls.expnorm(1-x, 1-mu, sigma1, lamb1, A*dA) +
+                cls.expnorm(x+1, 1-mu, sigma2, lamb2, A*(1-dA)))
 
     @classmethod
     def classificate(cls, data, plot=False, ax=None):
@@ -273,36 +255,44 @@ class Tools():
                 return 'Bimodal', mu, A
 
     @classmethod
-    def prefit(cls, data, pdf_type, mu, A, plot=False, ax=None):
-        y, x = np.histogram(data, bins='doane', density=True)
+    def prefit(cls, data, pdf_type, mu, A, plot=False, ax=None, hist=True):
+        y, x = np.histogram(data, bins='doane', density=True, range=(-1,1))
         x = (x[1:] + x[:-1]) / 2
 
         if plot:
             if ax is None:
                 plt.ion()
                 fig, ax = plt.subplots(1)
+            if hist:
+                ax.hist(data, bins='doane', normed=True, range=(-1,1), label='Data')
 
         if pdf_type == 'Bimodal':
             # params: mu, sigma1, sigma2, A, dA
             par0 = [mu, 0.10, 0.10, A, 0.5]
+            
             if -0.1 < mu < 0.1:
                 mins = [0, 0.01, 0.01, 0.0000, 0.45]
                 maxs = [1, 1.00, 1.00, np.inf, 0.55]
             elif -0.3 < mu < 0.3:
                 mins = [0, 0.01, 0.01, 0.0000, 0.35]
                 maxs = [1, 1.00, 1.00, np.inf, 0.65]
-            else:
+            elif -0.5 < mu < 0.5:
                 mins = [0, 0.01, 0.01, 0.0000, 0.20]
                 maxs = [1, 1.00, 1.00, np.inf, 0.80]
+            else:
+                mins = [0, 0.01, 0.01, 0.0000, 0.00]
+                maxs = [1, 1.00, 1.00, np.inf, 1.00]
             bounds = (mins, maxs)
             params, cov = curve_fit(cls.bimodal_gauss, x, y, par0, bounds=bounds)
+            
             if plot:
+                x = np.linspace(-1,1,200)
                 y1 = cls.gauss(x, params[0], params[1], params[3]*params[4])
                 y2 = cls.gauss(x, -params[0], params[2], params[3]*(1-params[4]))
                 y3 = y1 + y2
-                ax.plot(x, y1)
-                ax.plot(x, y2)
-                ax.plot(x, y3)
+                ax.plot(x, y1, label='Positive')
+                ax.plot(x, y2, label='Negative')
+                ax.plot(x, y3, label='Bimodal')
         elif pdf_type == 'Positive':
             # params: mu, sigma, A
             par0 = [mu, 0.10,      A]
@@ -310,9 +300,12 @@ class Tools():
             maxs = [+1, 0.50, np.inf]
             bounds = (mins, maxs)
             params, cov = curve_fit(cls.gauss, x, y, par0, bounds=bounds)
+            
             if plot:
+                x = np.linspace(-1,1,200)
                 y = cls.gauss(x, *params)
-                ax.plot(x, y)
+                ax.plot(x, y, label=pdf_type)
+                
         elif pdf_type == 'Negative':
             # params: mu, sigma, A
             par0 = [mu, 0.10,      A]
@@ -321,22 +314,27 @@ class Tools():
             bounds = (mins, maxs)
             params, cov = curve_fit(cls.gauss, x, y, par0, bounds=bounds)
             if plot:
+                x = np.linspace(-1,1,200)
                 y = cls.gauss(x, *params)
-                ax.plot(x, y)
+                ax.plot(x, y, label=pdf_type)
 
         sigma = np.sqrt(np.diag(cov))
 
-        return params, sigma
+        return 'Norm ' + pdf_type, params, sigmas
 
     @classmethod
-    def fit(cls, data, pdf_type, preparams, plot=False, ax=None):
+    def fit(cls, data, pdf_type, preparams, presigmas, plot=False, ax=None, hist=True):
         y, x = np.histogram(data, bins='doane', density=True, range=(-1,1))
         x = (x[1:] + x[:-1]) / 2
 
+        _, pdf_type = pdf_type.split(' ')
+        
         if plot:
             if ax is None:
                 plt.ion()
                 fig, ax = plt.subplots(1)
+            if hist:
+                ax.hist(data, bins='doane', normed=True, range=(-1,1), label='Histogram')
 
         if pdf_type == 'Bimodal':
             # params: mu, sigma1, sigma2, lamb1, lamb2, A, dA
@@ -352,33 +350,139 @@ class Tools():
             __mu, __s1, __s2, __A, __dA = pmax
 
             l1__ = 1.0
-            _l1_ = 49.0
-            __l1 = 50.0
+            _l1_ = 20.0
+            __l1 = 60.0
             l2__ = 1.0
-            _l2_ = 49.0
-            __l2 = 50.0
+            _l2_ = 20.0
+            __l2 = 60.0
 
             mins = [mu__, s1__, s2__, l1__, l2__, A__, dA__]
             par0 = [_mu_, _s1_, _s2_, _l1_, _l2_, _A_, _dA_]
             maxs = [__mu, __s1, __s2, __l1, __l2, __A, __dA]
             bounds = (mins, maxs)
             params, cov = curve_fit(cls.bimodal_expnorm, x, y, par0, bounds=bounds)
-            print(params)
+            
             if plot:
-                y1 = cls.expnorm(x,   params[0], params[1], params[3], params[5]*params[6])
-                y2 = cls.expnorm(1-x, 1+params[0], params[2], params[4], params[5]*(1-params[6]))
-                y3 = y1 + y2
+                x = np.linspace(-1,1,200)
+                y = cls.bimodal_expnorm(x, *params)
+                ax.plot(x,y, label='Bimodal')
 
-                ax.plot(x, y1, label='A')
-                ax.plot(x, y2, label='B')
-                ax.plot(x, y3, label='C')
+                y1 = cls.expnorm(1-x, 1-params[0], params[1], params[3], params[5]*params[6])
+                y2 = cls.expnorm(x+1, 1-params[0], params[2], params[4], params[5]*(1-params[6]))
+
+                ax.plot(x, y1, label='Positive')
+                ax.plot(x, y2, label='Negative')
 
                 ax.legend(loc='best')
-        elif pdf_type == 'Positive':
-            return
-        elif pdf_type == 'Negative':
-            return
+                
+        elif pdf_type in ('Positive','Negative'):
+            # params: mu, sigma, lamb, A
 
-        #sigma = np.sqrt(np.diag(cov))
+            p0 = np.asarray(preparams)
+            p0[0] = 1+p0[0]
+            pmax = p0 * 1.2
+            pmin = p0 * 0.8
+            p0 = p0.tolist()
+            pmax = pmax.tolist()
+            pmin = pmin.tolist()
 
-        return params#, sigma
+            mu__, s1__, A__, = pmin
+            _mu_, _s1_, _A_, = p0
+            __mu, __s1, __A, = pmax
+
+            l__ = 1.0
+            _l_ = 20.0
+            __l = 50.0
+
+            mins = [mu__, s1__, l__, A__]
+            par0 = [_mu_, _s1_, _l_, _A_]
+            maxs = [__mu, __s1, __l, __A]
+            bounds = (mins, maxs)
+            if pdf_type == 'Negative':
+                params, cov = curve_fit(cls.expnorm, 1+x, y, par0, bounds=bounds)
+            else:
+                params, cov = curve_fit(cls.expnorm, 1-x, y, par0, bounds=bounds)
+
+            if plot:
+                x = np.linspace(-1,1,200)
+                if pdf_type == 'Negative':
+                    y = cls.expnorm(1+x, params[0], params[1], params[2], params[3])
+                else:
+                    y = cls.expnorm(1-x, params[0], params[1], params[2], params[3])
+                ax.plot(x,y, label=pdf_type)
+                ax.legend(loc='best')
+
+        sigma = np.sqrt(np.diag(cov))
+
+        return 'ExponNorm ' + pdt_type, params, sigmas
+
+    @classmethod
+    def interpret(cls, pdf_type, params, sigmas):
+        if pdf_type == 'ExponNorm Bimodal':
+            mu, sigma1, sigma2, lamb1, lamb2, A, dA = params
+            mean1 = mu + 1/lamb1
+            mean2 = mu + 1/lamb2
+            sd1 = np.sqrt(sigma1 + (1/lamb1**2))
+            sd2 = np.sqrt(sigma2 + (1/lamb2**2))
+            return [mean1, -mean2], [sd1, sd2], [dA, 1-dA]
+        
+        elif pdf_type in ('ExponNorm Positive', 'ExponNorm Negative'):
+            mu, sigma, lamb, A = params
+            mean = abs(mu) + 1/lamb
+            sd = np.sqrt(sigma + (1/lamb**2))
+            if pdf_type == 'ExponNorm Positive':
+                return [0.0, +mean], [1.0, sd], [0.0, 1.0]
+            else:
+                return [-mean, 0.0], [sd, 1.0], [1.0, 0.0]
+            
+        elif pdf_type == 'Norm Bimodal':
+            mu, sigma1, sigma2, A, dA = params
+            return [mu, -mu], [sigma1, sigma2], [dA, 1-dA]
+        elif pdf_type in ('Norm Positive', 'Norm Negative'):
+            mu, sigma, A = params
+            mean = abs(mu)
+            sd = np.sqrt(sigma)
+            if pdf_type == 'Norm Positive':
+                return [0.0, +mean], [0.0, sd], [0.0, 1.0]
+            else:
+                return [-mean, 0.0], [sd, 0.0], [1.0, 0.0]
+        else:
+            raise ValueError
+    
+class Result():
+    def __init__(self, sample_name):
+        self._sample_name = sample_name
+        self._cls_params = None
+        self._pre_params = None
+        self._fit_params = None
+
+    @property
+    def magnet_array(self):
+        sample = Sample.load(self._sample_name)
+        return sample.magnet
+
+    @property
+    def energy_array(self):
+        sample = Sample.load(self._sample_name)
+        return sample.energy
+    
+    def fit(self, cls_ax=None, prefit_ax=None, fit_ax=None):
+        plot_cls = False
+        plot_prefit = False
+        plot_fit = False
+        
+        magnet = self.magnet_array
+
+        if cls_ax is not None: plot_cls=True
+        cls_params = Tools.classificate(magnet, plot=plot_cls, ax=cls_ax)
+
+        if prefit_ax is not None: plot_prefit=True
+        pre_params = Tools.prefit(magnet, *cls_params, plot=plot_prefit, ax=prefit_ax)
+
+        if fit_ax is not None: plot_fit=True
+        fit_params = Tools.fit(magnet, *pre_params, plot=plot_fit, ax=fit_ax)
+
+        self._cls_params = cls_params
+        self._pre_params = pre_params
+        self._fit_params = fit_params
+        
--- src/prueba5.py
+++ src/prueba5.py
@@ -17,22 +17,32 @@ import time
 sim1 = Simulation.load('../data/simulations/TestSim0.npy')
 analysis = Analysis(sim1)
 
-def test(fig, ax, result, state):
+def test(fig, axs, result, state):
     magnet = result.magnet_array/1024
-    ax.cla()
 
-    pdf_type, mu, A = Tools.classificate(magnet, plot=True, ax=ax)
+    for ax in axs:
+        ax.cla()
+
+    pdf_type, mu, A = Tools.classificate(magnet, plot=True, ax=axs[0])
     print(pdf_type, mu, A)
 
-    ax.axvline(mu, color='r', lw=5, alpha=0.5)
+    axs[0].axvline(mu, color='r', lw=5, alpha=0.5)
     if pdf_type == 'Bimodal':
-        ax.axvline(-mu, color='r', lw=5, alpha=0.5)
+        axs[0].axvline(-mu, color='r', lw=5, alpha=0.5)
 
-    params, sigma = Tools.prefit(magnet, pdf_type, mu, A, plot=True, ax=ax)
-#    params = Tools.fit(magnet, pdf_type, params, plot=True, ax=ax)
-    ax.relim()
-    ax.autoscale()
+    try:
+        params, sigma = Tools.prefit(magnet, pdf_type, mu, A, plot=True, ax=axs[1])
+    except:
+        pass
+    try:
+        params = Tools.fit(magnet, pdf_type, params, plot=True, ax=axs[2])
+    except:
+        pass
+
+    for ax in axs:
+        ax.relim()
+        ax.autoscale()
     plt.draw()
 
 
-analysis.subplot(test, 1,1)
+analysis.subplot(test, 3,1)
